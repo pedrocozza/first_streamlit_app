e324b911c64c542c984a8256be135b8ae9cf7633
pedrocozza <145564580+pedrocozza@users.noreply.github.com>
2023-09-21 12:55:58 +0200

    Update streamlit_app.py
    fixed order

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,16 +15,15 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-#New section to display fruitvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-
-
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+#New section to display fruitvice api response
+streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
